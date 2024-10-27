5f24983ca22733ed305f877dd735ad06c7e97e1f
gxlvezzz <you@example.com>
2024-10-28 00:10:57 +0100

    he hecho el apartado A

--- PECLGalvezEsquivelJulian/PECLGalvezEsquivelJulian/src/Source_FIles/Gestor.cpp
+++ PECLGalvezEsquivelJulian/PECLGalvezEsquivelJulian/src/Source_FIles/Gestor.cpp
@@ -1,19 +1,30 @@
 #include <src/Header_Files/Gestor.hpp>
 #include <iostream>
-#include "src/Source_Files/Proceso.cpp"
+#include <src/Header_Files/Proceso.hpp>
 using namespace std;
 
 	Pila pila;
 	Proceso proceso;
 
 void Gestor::genera12Procesos(){
+	if(pila.getLongitud()<48){
 	for (int i=0; i<12; i++){
 	proceso.crearProceso();
 	pila.insertar(proceso);
-	}	
+		}		
+	}else{
+		return;
+	}
 }
 
 int Gestor::ProcesosEnPila(){
 	int procesos_en_pila = pila.getLongitud();
 	return procesos_en_pila;
+}
+Gestor::Gestor(){
+		
+}
+
+Gestor::~Gestor(){
+	
 }
